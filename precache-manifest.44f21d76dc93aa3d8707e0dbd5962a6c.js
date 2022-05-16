df50f4f72a3f3a004c4b7564f88e34b5e4fa8e21
Rendymuktiis <rendytrading06@gmail.com>
2022-05-16 17:03:05 +0700

    Updates

---
+++ precache-manifest.44f21d76dc93aa3d8707e0dbd5962a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9617715cee0a1070acf7133ededdbddb",
+    "url": "/myporto/index.html"
+  },
+  {
+    "revision": "dd2be1933304fa775767",
+    "url": "/myporto/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ac9b669701e4aed634f7",
+    "url": "/myporto/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "dd2be1933304fa775767",
+    "url": "/myporto/static/js/2.236fbac8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/myporto/static/js/2.236fbac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9b669701e4aed634f7",
+    "url": "/myporto/static/js/main.94e30c37.chunk.js"
+  },
+  {
+    "revision": "85eb376635c24b7bfebc",
+    "url": "/myporto/static/js/runtime-main.b0c2bfcb.js"
+  }
+]);
